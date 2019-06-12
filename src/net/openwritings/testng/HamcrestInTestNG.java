119792baf8576de82725263ee2056905743ac55e
limelime <limelime@users.noreply.github.com>
2019-06-12 22:23:43 +0000

    Add more hamcrest examples.

--- src/net/openwritings/testng/HamcrestInTestNG.java
+++ src/net/openwritings/testng/HamcrestInTestNG.java
@@ -2,24 +2,22 @@ package net.openwritings.testng;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.testng.annotations.Test;
 
 public class HamcrestInTestNG
 {
-  @Test
-  public void useHamcrestToAssertStringContains()
-  {
-    String actual = "Assert string containing something.";
-    assertThat(actual, containsString("Not found"));
+    @Test(description="Negative test containsString().")
+    public void containsStringNegativeTest(){
+        String actual = "Assert string containing something.";
+        assertThat(actual, containsString("Not found"));
+    }
     
-    /**
-     * OUTPUT:
-          java.lang.AssertionError: 
-          Expected: a string containing "Not found"
-               but: was "Assert string containing something."
-            at org.hamcrest.MatcherAssert.assertThat(MatcherAssert.java:20)
-            at org.hamcrest.MatcherAssert.assertThat(MatcherAssert.java:8)
-     */
-  }
+    @Test(description="Positive test is().")
+    public void isPositiveTest(){
+        String actual = "Xuan";
+        assertThat(actual, is(equalTo("Xuan")));
+    }
 }
