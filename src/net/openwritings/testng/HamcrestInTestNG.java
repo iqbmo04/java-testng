f595be79a97f579f8df8a0f2b4bab4b0671b967f
limelime <limelime@users.noreply.github.com>
2019-06-28 22:40:32 +0000

    Format.

--- src/net/openwritings/testng/HamcrestInTestNG.java
+++ src/net/openwritings/testng/HamcrestInTestNG.java
@@ -7,17 +7,16 @@ import static org.hamcrest.Matchers.is;
 
 import org.testng.annotations.Test;
 
-public class HamcrestInTestNG
-{
-    @Test(description="Negative test containsString().")
-    public void containsStringNegativeTest(){
-        String actual = "Assert string containing something.";
-        assertThat(actual, containsString("Not found"));
-    }
-    
-    @Test(description="Positive test is().")
-    public void isPositiveTest(){
-        String actual = "Xuan";
-        assertThat(actual, is(equalTo("Xuan")));
-    }
+public class HamcrestInTestNG {
+	@Test(description = "Negative test containsString().")
+	public void containsStringNegativeTest() {
+		String actual = "Assert string containing something.";
+		assertThat(actual, containsString("Not found"));
+	}
+
+	@Test(description = "Positive test is().")
+	public void isPositiveTest() {
+		String actual = "Xuan";
+		assertThat(actual, is(equalTo("Xuan")));
+	}
 }
